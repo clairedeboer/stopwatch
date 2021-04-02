72156adcdb4fa54d943e87d538fd8ff066426201
clairedeboer <claire.deboer@gmail.com>
2021-04-02 11:42:31 -0400

    working on centiseconds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [seconds, setSeconds] = useState(parseFloat("0.00"));
+  const [centiseconds, setCentiseconds] = useState(0);
+  const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
-  const [spanse, setSpanse] = useState(0); 
-  //get start value to show up as 0.00, 1.00, 2.00 . . . 
+  const [spanse, setSpanse] = useState(0);
+  //get start value to show up as 0.00, 1.00, 2.00 . . .
   //update every 100th of a second
   //what does it mean when you return a function from a useEffect
 
+  // 100 centiseconds = 1 second
+  // centiseconds shouldn't go up past 100
   const handleStart = () => {
     const interval = setInterval(() => {
-      setSeconds((seconds) => seconds + 1);
-    }, 1000)
-    setSpanse(interval); 
-    console.log('start', spanse)
+      setCentiseconds((centiseconds) => {
+        if (centiseconds < 100) {
+          return centiseconds + 1;
+        } else {
+          return 0
+        }
+      });
+    }, 10);
+    setSpanse(interval);
   };
 
   const handleStop = () => {
     setSeconds(0);
     clearInterval(spanse);
-    console.log('stop', spanse)
   };
 
   // useEffect(() => {
@@ -28,11 +35,12 @@ const App = () => {
   //   }, 1000);
   //   return () => clearInterval(interval);
   // }, []);
-  
 
   return (
     <div>
-      <header>{seconds} seconds have elapsed </header>
+      <header>
+        00:00.{centiseconds}
+      </header>
       <button onClick={handleStart}>Start</button>
       <button onClick={handleStop}>Stop</button>
     </div>

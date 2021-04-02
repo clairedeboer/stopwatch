de87be5fe8c0367a5a902091dea97553b1403cfb
clairedeboer <claire.deboer@gmail.com>
2021-04-02 18:18:11 -0400

    display data and refactor

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const App = () => {
   const [centiseconds, setCentiseconds] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
-  const [spanse, setSpanse] = useState(0);
-  //get start value to show up as 0.00, 1.00, 2.00 . . .
-  //update every 100th of a second
-  //what does it mean when you return a function from a useEffect
+  const [centiSpanse, setCentiSpanse] = useState(0);
+  const [secSpanse, setSecSpanse] = useState(0);
+  const [minSpanse, setMinSpanse] = useState(0);
 
-  // 100 centiseconds = 1 second
-  // centiseconds shouldn't go up past 100
   const handleStart = () => {
-    const interval = setInterval(() => {
+    const centiInterval = setInterval(() => {
       setCentiseconds((centiseconds) => {
         if (centiseconds < 100) {
           return centiseconds + 1;
         } else {
-          return 0
+          return 0;
         }
       });
     }, 10);
-    setSpanse(interval);
+    setCentiSpanse(centiInterval);
+
+    const secInterval = setInterval(() => {
+      setSeconds((seconds) => {
+        if (seconds < 60) {
+          return seconds + 1;
+        } else {
+          return 0;
+        }
+      });
+    }, 1000);
+    setSecSpanse(secInterval);
+
+    const minInterval = setInterval(() => {
+      setMinutes((minutes) => minutes + 1);
+    }, 60000);
+    setMinSpanse(minInterval);
   };
 
   const handleStop = () => {
+    setCentiseconds(0);
     setSeconds(0);
-    clearInterval(spanse);
+    setMinutes(0);
+    clearInterval(centiSpanse);
+    clearInterval(secSpanse);
+    clearInterval(minSpanse);
   };
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setSeconds(seconds => seconds + 1)
-  //   }, 1000);
-  //   return () => clearInterval(interval);
-  // }, []);
 
   return (
     <div>
       <header>
-        00:00.{centiseconds}
+        <MinutesDisplay minutes={minutes} />
+        <SecondsDisplay seconds={seconds} />
+        <CentisecondsDisplay centiseconds={centiseconds} />{" "}
       </header>
       <button onClick={handleStart}>Start</button>
       <button onClick={handleStop}>Stop</button>
@@ -47,6 +59,16 @@ const App = () => {
   );
 };
 
-//once seconds get to 60, change minutes to +1 and reset seconds to 00
+const MinutesDisplay = ({ minutes }) => {
+  return <span>{minutes < 10 ? `0${minutes}` : minutes}:</span>;
+};
+
+const SecondsDisplay = ({ seconds }) => {
+  return <span>{seconds < 10 ? `0${seconds}` : seconds}.</span>;
+};
+
+const CentisecondsDisplay = ({ centiseconds }) => {
+  return <span>{centiseconds < 10 ? `0${centiseconds}` : centiseconds} </span>;
+};
 
 export default App;
